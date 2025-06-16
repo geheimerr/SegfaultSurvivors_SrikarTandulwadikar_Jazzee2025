72790ddc9ddd648d3832d6c2ad44d8e380d52922
Tejas Shandilya Sharma <tejas.shandilya24@gmail.com>
2025-06-16 21:06:14 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-// No backend authentication for now. Only prevent default form submission and show a toast.
-
-document.getElementById('login-form').addEventListener('submit', function (e) {
-    e.preventDefault();
-    // Redirect to portal selection page
-    window.location.href = 'portal.html';
-});
